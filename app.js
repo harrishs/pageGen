66042f020bbd0e1080054e44f978896e010eaf7d
Harrish Suhumar <harrish.s@hotmail.ca>
2021-01-30 19:11:35 -0500

    initial serserver setup

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+const mongoose = require("mongoose");
+require("dotenv").config();
+const cors = require("cors");
+const port = process.env.PORT || 3030;
+
+const app = express();
+app.use(cors());
+
+app.set("view engine", "ejs");
+app.set("views", "views");
+
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, "public")));
+
+
+mongoose
+  .connect(process.env.MONGO, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(port, () => {
+        console.log("Server running on " + port);
+    })
+  })
+  .catch((err) => {
+    throw err;
+  });
